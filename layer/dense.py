c6b6050320b6f953bc469671ae06d6f623fc92fa
dnsgh <yhboo@dsp.snu.ac.kr>
2017-08-15 16:57:22 +0900

    no message

--- layer/dense.py
+++ layer/dense.py
@@ -1,15 +1,34 @@
 import torch
-from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.nn as nn
 from torch.nn.parameter import Parameter
+import numpy as np
 
-class Dense(torch.nn.Module):
+class Dense(nn.Module):
     def __init__(self, D_in, D_out, use_bias = True):
         super(Dense, self).__init__()
-        self.d = torch.nn.Linear(D_in, D_out, use_bias)
+        #self.d = nn.Linear(D_in, D_out, use_bias)
+
+        self.D_in = D_in
+        self.D_out = D_out
+        self.use_bias = use_bias
+
+        self.weight = Parameter(torch.Tensor(D_in, D_out))
+        if use_bias:
+            self.bias = Parameter(torch.Tensor(D_out))
+        else:
+            self.register_parameter('bias', None)
+
+        ##initialization
+        variance = np.sqrt(2.0/(D_in + D_out))
+        self.weight.data.normal_(0.0, variance)
+        if use_bias:
+            self.bias.data.fill_(0.0)
 
 
     def forward(self, x):
-        return self.d(x)
+        if self.use_bias:
+            return F.m
 
 
     
--- module_test.py
+++ module_test.py
@@ -1,6 +1,17 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.nn.parameter import Parameter
 import numpy as np
 
 
-print('hello world!')
-print('please')
-print('done?')
+x_np = np.asarray([[1,2],[3,4]], dtype = 'float32')
+x_tc = torch.from_numpy(x_np)
+y_tc = torch.from_numpy(x_np)
+
+print(x_np)
+print(x_tc)
+
+x_tc.mm(y_tc)
+
+print(x_tc)
